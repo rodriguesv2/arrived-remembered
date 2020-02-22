4fecec3424e520c80c425a5947610f273baca036
Rubens Rodrigues <rubensrodriguesv2@gmail.com>
2020-02-22 19:39:30 -0300

    successful place autocomplete test

--- app/src/main/java/br/com/rubensrodrigues/arrived_remembered/ui/TesteActivity.kt
+++ app/src/main/java/br/com/rubensrodrigues/arrived_remembered/ui/TesteActivity.kt
@@ -1,5 +1,7 @@
 package br.com.rubensrodrigues.arrived_remembered.ui
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.rubensrodrigues.arrived_remembered.R
@@ -7,6 +9,7 @@ import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.widget.Autocomplete
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
+import kotlinx.android.synthetic.main.activity_teste.*
 
 class TesteActivity : AppCompatActivity() {
 
@@ -14,9 +17,7 @@ class TesteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teste)
 
-
-        if (!Places.isInitialized())
-            Places.initialize(this, getString(R.string.api_key))
+        if (!Places.isInitialized()) Places.initialize(this, getString(R.string.api_key))
 
         val intentAutoComplete = Autocomplete.IntentBuilder(
             AutocompleteActivityMode.OVERLAY,
@@ -24,5 +25,24 @@ class TesteActivity : AppCompatActivity() {
         ).build(this)
 
         startActivityForResult(intentAutoComplete, 1001)
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1001 && resultCode == Activity.RESULT_OK){
+            data?.let {
+                val place = Autocomplete.getPlaceFromIntent(it)
+                var str = ""
+                str += "ID: ${place.id}\n\n"
+                str += "NAME: ${place.name}\n\n"
+                str += "ADDRESS: ${place.address}\n\n"
+                str += "CORD: ${place.latLng}"
+
+                textview.text = str
+            }
+        }
     }
 }
