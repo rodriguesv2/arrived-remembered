2c3d65eb43221c696419f298472ff902d0ed3818
Rubens Rodrigues <rubensrodriguesv2@gmail.com>
2020-02-22 21:38:51 -0300

    permission helper created

--- app/src/main/java/br/com/rubensrodrigues/arrived_remembered/ui/TesteActivity.kt
+++ app/src/main/java/br/com/rubensrodrigues/arrived_remembered/ui/TesteActivity.kt
@@ -1,33 +1,58 @@
 package br.com.rubensrodrigues.arrived_remembered.ui
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import br.com.rubensrodrigues.arrived_remembered.R
+import br.com.rubensrodrigues.arrived_remembered.util.PermissionHelper
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.widget.Autocomplete
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import kotlinx.android.synthetic.main.activity_teste.*
+import org.jetbrains.anko.toast
 
 class TesteActivity : AppCompatActivity() {
+
+    private val REQUEST_LOCATION = 1002
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teste)
 
-        if (!Places.isInitialized()) Places.initialize(this, getString(R.string.api_key))
+//        if (!Places.isInitialized()) Places.initialize(this, getString(R.string.api_key))
+//
+//        val intentAutoComplete = Autocomplete.IntentBuilder(
+//            AutocompleteActivityMode.OVERLAY,
+//            listOf(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG)
+//        ).build(this)
+//
+//        startActivityForResult(intentAutoComplete, 1001)
 
-        val intentAutoComplete = Autocomplete.IntentBuilder(
-            AutocompleteActivityMode.OVERLAY,
-            listOf(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG)
-        ).build(this)
-
-        startActivityForResult(intentAutoComplete, 1001)
-
+        PermissionHelper.checkForPermission(this, Manifest.permission.ACCESS_FINE_LOCATION,
+            REQUEST_LOCATION
+        ){
+            showCoordination()
+        }
 
     }
+
+    @SuppressLint("MissingPermission")
+    private fun showCoordination() {
+        val systemService = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val location = systemService.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        textview.text = "${location.latitude.toFloat()}, ${location.longitude.toFloat()}"
+    }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -42,6 +67,25 @@ class TesteActivity : AppCompatActivity() {
                 str += "CORD: ${place.latLng}"
 
                 textview.text = str
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode){
+            REQUEST_LOCATION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    showCoordination()
+                } else {
+                    toast("Permissão a localização negada")
+                    finish()
+                }
             }
         }
     }
---
+++ app/src/main/java/br/com/rubensrodrigues/arrived_remembered/util/PermissionHelper.kt
@@ -0,0 +1,27 @@
+package br.com.rubensrodrigues.arrived_remembered.util
+
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+
+object PermissionHelper {
+
+    private fun shouldAskPermission(context: Context, permission: String): Boolean{
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED)
+                return true
+        }
+
+        return false
+    }
+
+    fun checkForPermission(context: Context, permission: String, requestCode: Int, permissionAlreadyGranted: (() -> Unit)? = null){
+        if (shouldAskPermission(context, permission)){
+            ActivityCompat.requestPermissions(context as Activity, listOf(permission).toTypedArray(), requestCode)
+        } else {
+            permissionAlreadyGranted?.invoke()
+        }
+    }
+}
